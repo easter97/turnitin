c48f91de397ac1f6939fda6334f7b3f05f3f78ea
Amanda Easter <me@amandaeaster.com>
2022-02-27 14:48:29 -0600

    Initialized and created delay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SmallExerciseComponent} from './small-exercise/small-exercise.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'small-exercise', component: SmallExerciseComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SmallExerciseComponent } from './small-exercise/small-exercise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SmallExerciseComponent
   ],
   imports: [
     BrowserModule,
